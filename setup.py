9be3b2b52bcd75ce6bff424e62d1b554c861a999
asherbar <asherbare@gmail.com>
2017-05-01 08:37:03 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='json_pp',
+    version='0.0.1',
+    packages=['parser', 'parser.unit_test'],
+    url='https://github.com/asherbar/json-plus-plus',
+    license='MIT',
+    author='asherbar',
+    author_email='asherbare@gmail.com',
+    description='An extension of JSON with an emphasis on reusability'
+)
