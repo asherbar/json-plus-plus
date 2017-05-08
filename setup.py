5bb51135d5644df00954d37e8e7388b3b0c605b7
asherbar <asherbare@gmail.com>
2017-05-08 10:15:04 +0300

    Fixed version file creation

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ VERSION = '0.0.2.4'
 CURR_DIR = os.path.dirname(os.path.abspath(__file__))
 
 with open(os.path.join(CURR_DIR, '__version__.py'), 'w') as version_fp:
-    version_fp.write('__version__ = {}\n'.format(VERSION))
+    version_fp.write('__version__ = "{}"\n'.format(VERSION))
 
 
 setup(
